ca40ad2fb1fc3a52a51364fb8ac96f76aa02efad
1234Asher <155654216+1234Asher@users.noreply.github.com>
2024-01-04 15:18:13 +0000

    coraçao

--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
-basic.forever(function () {
-    basic.showLeds(`
-        . # . # .
-        # # # # #
-        # # # # #
-        . # # # .
-        . . # . .
-        `)
-})
+basic.showLeds(`
+    . # . # .
+    # # # # #
+    # # # # #
+    . # # # .
+    . . # . .
+    `)
+basic.pause(5000)
