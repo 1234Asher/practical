03283c60890fd0918dc90689ecf500b3df9043e2
1234Asher <155654216+1234Asher@users.noreply.github.com>
2024-01-04 15:20:23 +0000

    Atualização main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,4 +5,11 @@ basic.showLeds(`
     . # # # .
     . . # . .
     `)
-basic.pause(5000)
+basic.pause(100)
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . # # # .
+    # . . . #
+    . . . . .
+    `)
